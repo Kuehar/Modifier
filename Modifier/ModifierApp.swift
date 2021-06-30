bc86636bff18f56eb67ca4a581916512380dacb9
kuehar <green95251@gmail.com>
2021-07-01 02:04:18 +0900

    Initial Commit

---
+++ Modifier/ModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModifierApp.swift
+//  Modifier
+//
+//  Created by kuehar on 2021/07/01.
+//
+
+import SwiftUI
+
+@main
+struct ModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
